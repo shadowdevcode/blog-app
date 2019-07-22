3ab017db8738964174c100f9d722007ce0c901b7
shadowdevcode <vijay.b.sehgal@gmail.com>
2019-07-22 23:45:27 +0530

    Update views.py

--- users/views.py
+++ users/views.py
@@ -9,29 +9,28 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Account created for {username}!")
+            return redirect("blog-home")
     else:
         form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form' : form})
+    return render(request, "users/register.html", {"form": form})
 
 
 @login_required
 def profile(request):
     u_form = UserUpdateForm(request.POST, instance=request.user)
-    p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+    p_form = ProfileUpdateForm(
+        request.POST, request.FILES, instance=request.user.profile
+    )
 
     if u_form.is_valid() and p_form.is_valid():
         u_form.save()
         p_form.save()
-        messages.success(request, f'Your Account Has Been Updated!')
-        return redirect('profile')
+        messages.success(request, f"Your Account Has Been Updated!")
+        return redirect("profile")
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
-    context = {
-        'u_form': u_form,
-        'p_form': p_form
-    }
-    return render(request, 'users/profile.html', context)
+    context = {"u_form": u_form, "p_form": p_form}
+    return render(request, "users/profile.html", context)
